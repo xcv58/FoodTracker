11eeb82aa99e2a63e804cbd5677add4b489daf7f
xcv58 <i@xcv58.com>
2017-10-09 22:53:21 -0700

    Add UIImageView

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // MARK: Properties
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         nameTextField.delegate = self
